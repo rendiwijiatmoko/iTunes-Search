850eda2e6cd593efda4a6733d085274a7483c4ea
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2022-08-24 20:11:23 +0700

    Initial Commit

---
+++ itunesApp/itunesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  itunesAppApp.swift
+//  itunesApp
+//
+//  Created by Rendi Wijiatmoko on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct itunesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
